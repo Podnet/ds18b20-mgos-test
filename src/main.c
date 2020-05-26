49f2e9780ad13d83862cfcc05fd914e269b10c08
hrithik098 <hrithiky328@gmail.com>
2020-05-26 11:40:19 +0530

    Added code for multiple temp sensor

--- src/main.c
+++ src/main.c
@@ -1,39 +1,35 @@
-/*
- * Copyright (c) 2014-2018 Cesanta Software Limited
- * All rights reserved
- *
- * Licensed under the Apache License, Version 2.0 (the ""License"");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an ""AS IS"" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #include "mgos.h"
+#include "DallasTemperature.h"
+#include "mgos_arduino_onewire.h"
 
-static void timer_cb(void *arg) {
-  static bool s_tick_tock = false;
-  LOG(LL_INFO,
-      ("%s uptime: %.2lf, RAM: %lu, %lu free", (s_tick_tock ? "Tick" : "Tock"),
-       mgos_uptime(), (unsigned long) mgos_get_heap_size(),
-       (unsigned long) mgos_get_free_heap_size()));
-  s_tick_tock = !s_tick_tock;
-#ifdef LED_PIN
-  mgos_gpio_toggle(LED_PIN);
-#endif
-  (void) arg;
+#define ONE_WIRE_BUS 2
+struct OneWire *ow;
+struct DallasTemperature *dt;
+int deviceCount = 0;
+float tempC;
+
+void get_temp_reading_cb()
+{
+  mgos_arduino_dt_request_temperatures(dt);
+  // Display temperature from each sensor
+  for (int i = 0; i < deviceCount; i++)
+  {
+    tempC = mgos_arduino_dt_get_tempc_by_index(dt, i);
+    LOG(LL_INFO, ("TCU: Temparture of  found : %.2f from %i device", tempC, i));
+  }
 }
 
-enum mgos_app_init_result mgos_app_init(void) {
-#ifdef LED_PIN
-  mgos_gpio_setup_output(LED_PIN, 0);
-#endif
-  mgos_set_timer(1000 /* ms */, MGOS_TIMER_REPEAT, timer_cb, NULL);
+enum mgos_app_init_result mgos_app_init(void)
+{
+  ow = mgos_arduino_onewire_create(ONE_WIRE_BUS);
+  dt = mgos_arduino_dt_create(ow);
+  mgos_arduino_dt_begin(dt);
+  deviceCount = mgos_arduino_dt_get_device_count(dt);
+  LOG(LL_INFO, ("TCU: Devices found : %i", deviceCount));
+  mgos_set_timer(
+      1000,
+      MGOS_TIMER_REPEAT,
+      get_temp_reading_cb,
+      NULL);
   return MGOS_APP_INIT_SUCCESS;
 }
